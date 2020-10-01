cc1276259860cee44cab3838632d0f8c79b7917e
GeorgeWL <georgelangham1990@gmail.com>
2020-10-01 17:36:13 +0100

    add contact page

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -72,7 +72,7 @@
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-    <li><a aria-current="{segment === 'contact' ? 'page' : undefined}" href="about">contact</a></li>
+    <li><a aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">contact</a></li>
 
 	</ul>
 </nav>
